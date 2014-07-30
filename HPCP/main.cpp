946d094674af895352d4562b4b656cc95ca2bb34
ITMA user <itma@172-10-20-109.lightspeed.clmasc.sbcglobal.net>
2014-07-30 09:07:56 +0100

    Initial Commit

---
+++ HPCP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HPCP
+//
+//  Created by ITMA user on 30/07/2014.
+//  Copyright (c) 2014 ITMA user. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
